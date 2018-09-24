89ca8532774d4f398848cf05a0bf34d4b42d820d
desxeda <desxeda@gmail.com>
2018-09-24 10:03:31 +0800

    Established database connection

---
+++ UIA/ConnectDB.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace UIA
+{
+    public class ConnectDB
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Source code reference: https://docs.microsoft.com/en-us/azure/sql-database/sql-database-connect-query-dotnet-visual-studio
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "your_server.database.windows.net";
+                builder.UserID = "your_user";
+                builder.Password = "your_password";
+                builder.InitialCatalog = "your_database";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+
+                    submitSQLQuery(connection,"Creating Tables...",createQueries());
+                    submitSQLQuery(connection,"Populating data...",insertDummyData());
+   
+    
+                }
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public static string createQueries()
+        {
+            return @" CREATE TABLE [user] (
+                [userID][int] NOT NULL,
+                [password] [varchar] (50) NULL,
+	            [fullname] [varchar] (50) NULL,
+	            [passportNo] [varchar] (50) NULL,
+	            CONSTRAINT PK_user PRIMARY KEY(userID)
+                ) 
+
+                CREATE TABLE[flight] (
+                    [flightID][int] NOT NULL,
+                    [timeofDepature] [varchar] (200) NULL,
+	                [timeofArrival] [varchar] (200) NULL,
+	                [destination] [varchar] (50) NULL,
+	                [status] [varchar] (50) NULL,
+	                CONSTRAINT PK_flight PRIMARY KEY(flightID)
+                )	
+
+                CREATE TABLE[reservation] (
+                    [fullname][varchar](50) NULL,
+	                [passportNo] [varchar] (50) NULL,
+	                [flightID] [int] NOT NULL references flight(flightID),
+	                [timeofDepature] [varchar] (200) NULL,
+	                [timeofArrival] [varchar] (200) NULL,
+	                [destination] [varchar] (50) NULL,
+                ) ";
+        }
+        public static string insertDummyData()
+        {
+            return @"INSERT INTO [dbo].[flight](
+                [flightID],
+	            [timeofDepature] ,
+	            [timeofArrival] ,
+	            [destination] ,
+	            [status]
+                ) VALUES(1, '14 January 2017 1:30pm' , '14 January 2017 2:45pm' ,'Moscow','Active');
+
+                INSERT INTO [dbo].[flight](
+                [flightID],
+	            [timeofDepature] ,
+	            [timeofArrival] ,
+	            [destination] ,
+	            [status]
+                ) VALUES(2, '14 January 2017 2:30pm' , '14 January 2017 4:45pm' ,'Frankfurt','Active');
+
+                INSERT INTO [dbo].[flight](
+                [flightID],
+	            [timeofDepature] ,
+	            [timeofArrival] ,
+	            [destination] ,
+	            [status]
+                ) VALUES(3, '15 January 2017 11:30am' , '15 January 2017 1:45pm' ,'Paris','Active');
+
+                INSERT INTO [dbo].[flight](
+                [flightID],
+	            [timeofDepature] ,
+	            [timeofArrival] ,
+	            [destination] ,
+	            [status]
+                ) VALUES(4, '16 January 2017 4:30pm' , '16 January 2017 8:15pm' ,'London','Active');
+
+                INSERT INTO [dbo].[flight](
+                [flightID],
+	            [timeofDepature] ,
+	            [timeofArrival] ,
+	            [destination] ,
+	            [status]
+                ) VALUES(5, '17 January 2017 10:30pm' , '17 January 2017 7:00am' ,'Bejing','Active');";
+        }
+        static void submitSQLQuery(SqlConnection connection,string tsqlPurpose,string tsqlSourceCode,string parameterName = null,string parameterValue = null){
+            Console.WriteLine();
+            Console.WriteLine("=================================");
+            Console.WriteLine("T-SQL to {0}...", tsqlPurpose);
+
+            using (var command = new SqlCommand(tsqlSourceCode, connection))
+            {
+                if (parameterName != null)
+                {
+                    command.Parameters.AddWithValue(  // Or, use SqlParameter class.
+                       parameterName,
+                       parameterValue);
+                }
+                int rowsAffected = command.ExecuteNonQuery();
+                Console.WriteLine(rowsAffected + " = rows affected.");
+                }
+            }
+        } // EndOfClass
+    }//end of ConnectDB class
+}
