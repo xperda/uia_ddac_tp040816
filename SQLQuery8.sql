a3329793ddffcbed2570416f625e50af3d9a248c
desxeda <desxeda@gmail.com>
2018-10-01 08:53:59 +0800

    Deleted temp queries

--- SQLQuery8.sql
+++
@@ -1,8 +0,0 @@
-﻿ INSERT INTO [reservation]([fullname],[passportNo],[flightID],[timeofDepature],[timeofArrival],[destination])
-                    SELECT u.[fullname],u.[passportNo],f.[flightID],f.[timeofDepature],f.[timeofArrival],f.[destination]
-                    FROM[user] u CROSS JOIN[flight] f 
-                    WHERE u.username = 'demouser' and f.flightID = 2;
- 
- SELECT * FROM reservation WHERE EXISTS (SELECT fullname from [user] where username = 'demouser');
-
- SELECT * FROM reservation;
